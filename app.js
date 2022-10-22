3af188afdeb07b51c7b78cd70998389af74e2587
TomasCalabria <totocalabria.tc@gmail.com>
2022-10-22 15:30:37 -0300

    eliminando logs

--- app.js
+++ app.js
@@ -19,9 +19,7 @@ mongoose.connect(`${process.env.MONGO_URI}`)
 .catch((err)=>{console.log(err)})
 
 
-app.get('/',(req,res)=>{
-    res.send('holaaaa')
-})
+
 app.use('/users',(users))
 app.use('/expenses',expenses)
 
