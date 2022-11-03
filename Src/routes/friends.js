e42588fe35be93c5b7483331fdab141820180d03
TomasCalabria <totocalabria.tc@gmail.com>
2022-11-03 18:39:48 -0300

    Continuando funcion agregar amigos

--- Src/routes/friends.js
+++ Src/routes/friends.js
@@ -1,7 +1,8 @@
 var  express = require('express');
 var  router = express.Router();
 const User = require('../Models/User');
-const {verifyToken,verifyAuthorization}=require('../Middlewares/verifyToken')
+const {verifyToken,verifyAuthorization}=require('../Middlewares/verifyToken');
+const { $where } = require('../Models/User');
 
 
 router.get('/:id/all',verifyAuthorization,async(req,res)=>{
@@ -16,25 +17,69 @@ router.get('/:id/all',verifyAuthorization,async(req,res)=>{
 })
 
 router.post('/add',async(req,res)=>{
-  try{ 
-    const user = await User.findOne({},{id:req.body.id})
-if(user){
-      const friend= await User.findOne({},{username:req.body.friend})
-      res.status(200).json(`Friend Added corectly ${friend}` )
-}
-else{
-  res.status(401).json('Please review the username you entered')
+  const friend= await User.findOne({username:req.body.friend})
+  if(friend){
+    try{ 
+      const user=await User.findById(req.body.id)
+      user.friends.find((x)=>{
+        if(x.friend_username==req.body.friend){
+          res.status(401).json({
+            status: "Error",
+            message: `Sorry, your friend has been already added`,
+            data: []
+                      
+          })
+        }
+          else{
+            User.findByIdAndUpdate(req.body.id,{
+        
+              $addToSet:{friends:{
+                friend_id:friend.id,
+                friend_username:friend.username
+              }}
+            },{upsert:true,safe:true})
+        
+            .then((result)=>{
+              res.status(200).json({
+                      status: "Success",
+                      message: `Friend has been added correctly! ${result}`,
+                      data: result
+                      
+              })
+            console.log(result)
+      
+            })
+          }
+        })
+    
+      .catch((err)=>{
+         res.status(500).json({
+          status: "Failed",
+          message: "Database Error",
+          data: err
+         })
+        })
+      }
 
-}
-  
-  }
+
   catch(err){
     res.status(401).json('Sorry there has been an internal server error')
   }
+}
+else{
 
 
+  res.status(404).json({
+      status: "Failed",
+      message: "We are sorry but the username was not found",
+      data:null
+  })
+  console.log(`There has been an failed attempt of adding a new user. \nUser: ${req.headers.username} `)
+
+}
 
 
+  
 })
 
 
